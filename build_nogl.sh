b15c34a581f8a2612f6ba94ef4529212f16f3773
Maraneshi <1661732+Maraneshi@users.noreply.github.com>
2017-02-09 16:21:12 +0100

    fixed build without gl

--- build_nogl.sh
+++ build_nogl.sh
@@ -1,5 +1,5 @@
 FILES="*.cpp *.cu"
-FLAGS="-std=c++11 -O3 -arch=sm_35 -res-usage -lineinfo"
+FLAGS="-std=c++11 -O3 -arch=sm_35 -res-usage -lineinfo -DCM_NOGL"
 FOLDERS="-Iinclude -Llib"
 LIBS=""
 TARGET="bin/mandelbrot"
